33a43b983577b7b96fbdf9f443589d4e3176de4c
Sanders003 <kvshemanthkumar003@gmail.com>
2025-08-14 23:40:52 +0530

    updated README

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "website_url" {
+output website_url {
   description = "Access your Flask app at this URL"
   value       = "http://${aws_instance.flask_app.public_ip}:8000"
 }
 
-output "s3bucket" {
+output s3bucket {
   description = "Access for s3 bucket only from instance"
   value       = "aws s3 cp s3://${aws_s3_bucket.data_source_bucket.id}/data.txt"
 }
